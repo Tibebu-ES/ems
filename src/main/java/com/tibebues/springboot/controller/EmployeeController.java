a252bf0456a6928c495686ce9f9144b6523831e4
Tibebu-ES <tibebuenyew@gmail.com>
2021-01-17 18:40:30 +0300

    Add Employee API implemented

--- src/main/java/com/tibebues/springboot/controller/EmployeeController.java
+++ src/main/java/com/tibebues/springboot/controller/EmployeeController.java
@@ -4,6 +4,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +25,12 @@ public class EmployeeController {
 	@GetMapping("/employees")
 	public List<Employee> getAllEmployees(){
 		return employeeRepository.findAll();
+	}
+	
+	//create employee
+	@PostMapping("/employees")
+	public Employee createEmploye(@RequestBody Employee employee) {
+		return employeeRepository.save(employee);
 	}
 
 }
