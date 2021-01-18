d89191c5a4917bae9574ac0c6a32e5b9a858afa9
Tibebu-ES <tibebuenyew@gmail.com>
2021-01-18 20:06:35 +0300

    Delete Employee Rest API implemented

--- src/main/java/com/tibebues/springboot/controller/EmployeeController.java
+++ src/main/java/com/tibebues/springboot/controller/EmployeeController.java
@@ -1,9 +1,13 @@
 package com.tibebues.springboot.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -58,6 +62,17 @@ public class EmployeeController {
 		Employee updatedEmployee = employeeRepository.save(oldEmployee);
 		return ResponseEntity.ok(updatedEmployee);
 		
+	}
+	
+	//delete employee
+	@DeleteMapping("/employees/{id}")
+	public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable long id){
+		Employee employee = employeeRepository.findById(id)
+				.orElseThrow( ()-> new ResourceNotFoundException("Employee not found with id:"+id));
+		employeeRepository.delete(employee);
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("deleted", Boolean.TRUE);
+		return ResponseEntity.ok(response);
 	}
 
 }
